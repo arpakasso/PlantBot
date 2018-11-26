41c6b4390a3e5595f7e74d42238028c9d57c5b20
arpakasso <e.n.trinh@outlook.com>
2018-11-25 23:11:26 -0600

    serve_zone

--- server.py
+++ server.py
@@ -9,7 +9,10 @@ Purpose:
 from flask import Flask, request
 import os
 import requests
+import json
 from df_response_lib import fulfillment_response
+import api
+import re
 
 app = Flask(__name__)
 
@@ -18,8 +21,26 @@ app = Flask(__name__)
 def serve_homepage():
     return 'homepage'
 
+
 def serve_zone(param):
-    return param
+    city = param.get('geo-city') or ""
+    state = param.get('geo-state-us') or ""
+    geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?address="+city+","+state+"&location_type=GEOMETRIC_CENTER&key="+api.maps_api_key
+    geocode_resp = json.loads(requests.post(geocode_url).text)
+    lat = geocode_resp['results'][0]['geometry']['location']['lat']
+    lng = geocode_resp['results'][0]['geometry']['location']['lng']
+    rev_geocode_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng="+str(lat)+","+str(lng)+"&key="+api.maps_api_key
+    rev_geocode_resp = json.loads(requests.post(rev_geocode_url).text)
+    components = rev_geocode_resp['results'][0]['address_components']
+    zip_code = "75080"
+    for comp in components:
+        if 'postal_code' in comp['types']:
+            zip_code = comp['long_name']
+    zone_url = "https://phzmapi.org/"+zip_code+".json"
+    zone_resp = json.loads(requests.get(zone_url).text)
+    zone = re.sub('[^0-9]', '', zone_resp['zone'])
+    return "You live in Zone " + zone
+
 
 @app.route('/webhook', methods=['POST'])
 def serve_webhook():
@@ -33,9 +54,10 @@ def serve_webhook():
     if action == "findzone.findzone-custom":
         resp = fr.fulfillment_text(serve_zone(req.get('queryResult').get('parameters')))
     # return a fulfillment response
-    return fr.main_response(fulfillment_text=resp)
+    return json.dumps(fr.main_response(fulfillment_text=resp))
 
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, threaded=True)
+    # app.run(threaded=True)
